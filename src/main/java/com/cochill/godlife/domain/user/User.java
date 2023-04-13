7fd7e5ce9600b30673bf71960bfd5e0ee8cab75f
nimosarang <111734043+nimosarang@users.noreply.github.com>
2023-04-13 11:15:39 +0900

    v2

--- src/main/java/com/cochill/godlife/domain/user/User.java
+++ src/main/java/com/cochill/godlife/domain/user/User.java
@@ -4,19 +4,7 @@ public class User {
 
     private String username;
     private String password;
-    private String email;
-    private String firstName;
-    private String lastName;
-    private String phone;
-    private String address;
-    private String city;
-    private String state;
-    private String zip;
-    private String country;
-    private String role;
-    private String status;
-    private String created;
-    private String updated;
+
 
     public User() {
     }
